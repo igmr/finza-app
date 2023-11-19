d694eb4a7187a9900a14a96fae670e450dfdd57f
Ivan Martinez <igmrivan@gmail.com>
2023-11-19 12:45:22 -0600

    Updated api route file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BankController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ClassificationController;
+use App\Http\Controllers\Api\DebtController;
+use App\Http\Controllers\Api\SavingController;
+use App\Http\Controllers\Api\AccountController;
+use App\Http\Controllers\Api\BudgetController;
+use App\Http\Controllers\Api\IngressController;
+use App\Http\Controllers\Api\EgressController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +28,96 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/auth/login',          [AuthenticationController::class, 'login']);
+
+Route::prefix('user')->group(function () {
+    Route::get('/',                [UserController::class, 'index']);
+    Route::get('/{id}',            [UserController::class, 'show']);
+    Route::post('/',               [UserController::class, 'store']);
+    Route::put('/{id}',            [UserController::class, 'update']);
+    Route::delete('/{id}',         [UserController::class, 'destroy']);
+    Route::delete('/{id}/restore', [UserController::class, 'restore']);
+});
+
+Route::prefix('bank')->group(function () {
+    Route::get('/',                [BankController::class, 'index']);
+    Route::get('/{id}',            [BankController::class, 'show']);
+    Route::post('/',               [BankController::class, 'store']);
+    Route::put('/{id}',            [BankController::class, 'update']);
+    Route::delete('/{id}',         [BankController::class, 'destroy']);
+    Route::delete('/{id}/restore', [BankController::class, 'restore']);
+});
+
+Route::prefix('category')->group(function () {
+    Route::get('/',                [CategoryController::class, 'index']);
+    Route::get('/{id}',            [CategoryController::class, 'show']);
+    Route::post('/',               [CategoryController::class, 'store']);
+    Route::put('/{id}',            [CategoryController::class, 'update']);
+    Route::delete('/{id}',         [CategoryController::class, 'destroy']);
+    Route::delete('/{id}/restore', [CategoryController::class, 'restore']);
+});
+
+Route::prefix('classification')->group(function () {
+    Route::get('/',                [ClassificationController::class, 'index']);
+    Route::get('/{id}',            [ClassificationController::class, 'show']);
+    Route::post('/',               [ClassificationController::class, 'store']);
+    Route::put('/{id}',            [ClassificationController::class, 'update']);
+    Route::delete('/{id}',         [ClassificationController::class, 'destroy']);
+    Route::delete('/{id}/restore', [ClassificationController::class, 'restore']);
+});
+
+Route::prefix('debt')->group(function () {
+    Route::get('/',                [DebtController::class, 'index']);
+    Route::get('/{id}',            [DebtController::class, 'show']);
+    Route::post('/',               [DebtController::class, 'store']);
+    Route::put('/{id}',            [DebtController::class, 'update']);
+    Route::delete('/{id}',         [DebtController::class, 'destroy']);
+    Route::delete('/{id}/restore', [DebtController::class, 'restore']);
+});
+
+Route::prefix('saving')->group(function () {
+    Route::get('/',                [SavingController::class, 'index']);
+    Route::get('/{id}',            [SavingController::class, 'show']);
+    Route::post('/',               [SavingController::class, 'store']);
+    Route::put('/{id}',            [SavingController::class, 'update']);
+    Route::delete('/{id}',         [SavingController::class, 'destroy']);
+    Route::delete('/{id}/restore', [SavingController::class, 'restore']);
+});
+
+Route::prefix('account')->group(function () {
+    Route::get('/',                [AccountController::class, 'index']);
+    Route::get('/{id}',            [AccountController::class, 'show']);
+    Route::post('/',               [AccountController::class, 'store']);
+    Route::put('/{id}',            [AccountController::class, 'update']);
+    Route::delete('/{id}',         [AccountController::class, 'destroy']);
+    Route::delete('/{id}/restore', [AccountController::class, 'restore']);
+});
+
+Route::prefix('budget')->group(function () {
+    Route::get('/',                [BudgetController::class, 'index']);
+    Route::get('/{id}',            [BudgetController::class, 'show']);
+    Route::post('/',               [BudgetController::class, 'store']);
+    Route::put('/{id}',            [BudgetController::class, 'update']);
+    Route::delete('/{id}',         [BudgetController::class, 'destroy']);
+    Route::delete('/{id}/restore', [BudgetController::class, 'restore']);
+});
+
+Route::prefix('ingress')->group(function () {
+    Route::get('/',                [IngressController::class, 'index']);
+    Route::get('/{id}',            [IngressController::class, 'show']);
+    Route::post('/',               [IngressController::class, 'store']);
+    Route::put('/{id}',            [IngressController::class, 'update']);
+    Route::delete('/{id}',         [IngressController::class, 'destroy']);
+    Route::delete('/{id}/restore', [IngressController::class, 'restore']);
+});
+
+Route::prefix('egress')->group(function () {
+    Route::get('/',                [EgressController::class, 'index']);
+    Route::get('/{id}',            [EgressController::class, 'show']);
+    Route::post('/',               [EgressController::class, 'store']);
+    Route::put('/{id}',            [EgressController::class, 'update']);
+    Route::delete('/{id}',         [EgressController::class, 'destroy']);
+    Route::delete('/{id}/restore', [EgressController::class, 'restore']);
 });
