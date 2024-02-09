70c75c98c6595a6bce9a34bf137b8de0c89eadc6
Ivan Martinez <igmrivan@gmail.com>
2024-02-08 20:09:45 -0600

    Update timezone for America/Mexico_City

--- config/app.php
+++ config/app.php
@@ -68,9 +68,10 @@ return [
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
     |
+    'timezone' => 'UTC',
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Mexico_City',
 
     /*
     |--------------------------------------------------------------------------
